cd583d4e8badb52d63ce8608769a7b1c7d2bcafb
AliceGl <alice2003b@gmail.com>
2021-09-29 23:04:55 +0300

    written function insertValueByKey

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,44 +6,66 @@ import java.nio.file.StandardOpenOption
 data class InputData(val command: String, val database : String,
                      val key : String?, val value : String?)
 
+const val MaxSize = (1 shl 16) - 1
+
+fun addLine(filePath: String, line: String) {
+    Files.write(Paths.get(filePath), (line + "\n").toByteArray(), StandardOpenOption.APPEND)
+}
+
 fun parseArgs(args: Array<String>) : InputData? {
     return InputData(args.getOrNull(0) ?: return null,
     args.getOrNull(1) ?: return null,
     args.getOrNull(2), args.getOrNull(3))
 }
 
+fun path(name: String) = "database/$name"
+
+fun getFilePath(name: String, index: Int) = path(name) + "/$index.txt"
+
+fun getInfoPath(name: String) = path(name) + "/info.txt"
+
+fun getIndex(name: String) = File(getInfoPath(name)).readText().toInt()
+
+fun createFile(name: String) {
+    val index = getIndex(name)
+    File(getFilePath(name, index)).createNewFile()
+    File(getInfoPath(name)).writeText((index + 1).toString())
+}
+
 fun newDatabase (name: String) {
-    check (!File("database/$name").exists()) { "Database $name already exist"}
-    File("database/$name").mkdir()
-    File("database/$name/info.txt").createNewFile()
-    File("database/$name/0.txt").createNewFile()
-    Files.write(Paths.get("database/$name/info.txt"),
-        "1".toByteArray(), StandardOpenOption.APPEND)
+    check (!File(path(name)).exists()) { "Database $name already exist"}
+    File(path(name)).mkdir()
+    File(getInfoPath(name)).createNewFile()
+    File(getFilePath(name, 0)).createNewFile()
+    File(getInfoPath(name)).writeText("1")
 }
 
-fun deleteDatabase (name: String) {
-    check (File("database/$name").exists()) { "Database $name doesn't exist"}
-    for (i in 0 until File("database/$name/info.txt").readLines()[0].toInt())
-        File("database/$name/$i.txt").delete()
-    File("database/$name/info.txt").delete()
-    File("database/$name").delete()
+fun deleteDatabase(name: String) {
+    check (File(path(name)).exists()) { "Database $name doesn't exist"}
+    for (i in 0 until getIndex(name))
+        File(getFilePath(name, i)).delete()
+    File(getInfoPath(name)).delete()
+    File(path(name)).delete()
 }
 
-fun getValue (database: String, key: String?) {
+fun getValue(database: String, key: String?) {
     check(key != null) {"Not enough arguments"}
-    check (File("database/$database").exists()) { "Database $database doesn't exist"}
+    check(File(path(database)).exists()) { "Database $database doesn't exist"}
     TODO()
 }
 
-fun insertValueByKey (database: String, key: String?, value: String?) {
+fun insertValueByKey(database: String, key: String?, value: String?) {
     check(key != null && value != null) {"Not enough arguments"}
-    check (File("database/$database").exists()) { "Database $database doesn't exist"}
-    TODO()
+    check(File(path(database)).exists()) { "Database $database doesn't exist"}
+    val filePath = getFilePath(database, getIndex(database) - 1)
+    addLine(filePath, "$key $value")
+    if (File(filePath).length() > MaxSize)
+        createFile(database)
 }
 
-fun removeKey (database: String, key: String?) {
+fun removeKey(database: String, key: String?) {
     check(key != null) {"Not enough arguments"}
-    check (File("database/$database").exists()) { "Database $database doesn't exist"}
+    check(File(path(database)).exists()) { "Database $database doesn't exist"}
     TODO()
 }
 
